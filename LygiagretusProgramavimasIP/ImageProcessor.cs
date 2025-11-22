cfbe2e35151115aef58c56b489b636e3cee42a84
ignas24 <ignaspyrantas@gmail.com>
2025-11-22 16:16:09 +0200

    Add project files.

---
+++ LygiagretusProgramavimasIP/ImageProcessor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LygiagretusProgramavimasIP
+{
+    internal class ImageProcessor
+    {
+        public static void GrayScaleImage(string imagePath, string resultDirectory)
+        {
+
+        }
+        public static List<string> ReadImageDirectory(string directoryPath)
+        {
+
+            return new List<string>();
+        }
+    }
+}
---
+++ LygiagretusProgramavimasIP/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace LygiagretusProgramavimasIP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string imageDirectory = "/TestCases/1";
+            int threadCount = 1;
+            List<string> imagePaths = ImageProcessor.ReadImageDirectory(imageDirectory);
+            imagePaths.AsParallel().WithDegreeOfParallelism(threadCount).ForAll(imagePath =>
+            {
+                ImageProcessor.GrayScaleImage(imagePath, "resultDirectory");
+            });
+        }
+    }
+}
