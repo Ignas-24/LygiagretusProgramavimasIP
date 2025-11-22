a9d919fee2bf5c3ef35e2fe7a1710c7e87a41fbc
ignas24 <ignaspyrantas@gmail.com>
2025-11-22 18:27:56 +0200

    Implemented grayscale logic and added function to read all pngs from a directory

--- LygiagretusProgramavimasIP/ImageProcessor.cs
+++ LygiagretusProgramavimasIP/ImageProcessor.cs
@@ -1,21 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SkiaSharp;
 
 namespace LygiagretusProgramavimasIP
 {
-    internal class ImageProcessor
+    internal static class ImageProcessor
     {
-        public static void GrayScaleImage(string imagePath, string resultDirectory)
+        public static void GrayscaleImage(string imagePath, string resultDirectory)
         {
+            string resultPath = Path.Combine(resultDirectory, Path.GetFileName(imagePath));
+            using var inputImage = SKBitmap.Decode(imagePath);
+            var grayScaleImage = new SKBitmap(inputImage.Width, inputImage.Height);
+            for(int x = 0; x < inputImage.Width; x++)
+            {
+                for(int y = 0; y < inputImage.Height; y++)
+                {
+                    var pixel = inputImage.GetPixel(x, y);
 
+                    byte grayValue = (byte)(0.299 * pixel.Red + 0.587 * pixel.Green + 0.114 * pixel.Blue);
+                    
+                    grayScaleImage.SetPixel(x, y, new SKColor(grayValue, grayValue, grayValue, pixel.Alpha));
+                }
+            }
+            using var surface = SKImage.FromPixels(grayScaleImage.PeekPixels());
+            using var outputData = surface.Encode(SKEncodedImageFormat.Png, 100);
+            using var outputStream = File.OpenWrite(resultPath);
+
+            outputData.SaveTo(outputStream);
         }
         public static List<string> ReadImageDirectory(string directoryPath)
         {
-
-            return new List<string>();
+            string[] pathsArr = Directory.GetFiles(directoryPath);
+            List<string> paths = new List<string>(pathsArr);
+            paths = paths.Where(path => path.EndsWith(".png")).ToList();
+            return paths;
         }
     }
 }
--- LygiagretusProgramavimasIP/Program.cs
+++ LygiagretusProgramavimasIP/Program.cs
@@ -1,16 +1,23 @@
-﻿
-namespace LygiagretusProgramavimasIP
+﻿namespace LygiagretusProgramavimasIP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string imageDirectory = "/TestCases/1";
-            int threadCount = 1;
+            string imageDirectory = "TestCases\\1";
+            string resultDirectory = "Results";
+            var fullResultPath = Path.Combine(Environment.CurrentDirectory, resultDirectory);
+            int threadCount = 3;
             List<string> imagePaths = ImageProcessor.ReadImageDirectory(imageDirectory);
+            if (Directory.Exists(fullResultPath))
+            {
+                Directory.Delete(fullResultPath, true);
+            }
+            var dir = Directory.CreateDirectory(fullResultPath);
+            string resultPath = dir.FullName;
             imagePaths.AsParallel().WithDegreeOfParallelism(threadCount).ForAll(imagePath =>
             {
-                ImageProcessor.GrayScaleImage(imagePath, "resultDirectory");
+                ImageProcessor.GrayscaleImage(imagePath, resultDirectory);
             });
         }
     }
