b0afd008ab578adcdab4cc9f149624658e566539
ignas24 <ignaspyrantas@gmail.com>
2025-11-24 19:06:36 +0200

    Main changes for testing

--- LygiagretusProgramavimasIP/Program.cs
+++ LygiagretusProgramavimasIP/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             List<TimeSpan> timings = new List<TimeSpan>();
-            int runCount = 1;
+            int runCount = 2;
             for (int i = 0; i < runCount; i++)
             {
                 string projectDir = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(AppContext.BaseDirectory).FullName).FullName).FullName).FullName;
@@ -13,7 +13,7 @@
                 string resultDirectory = "Results";
                 var fullResultPath = Path.Combine(projectDir, resultDirectory);
                 var fullImageDirectory = Path.Combine(projectDir, imageDirectory);
-                int threadCount = 16;
+                int threadCount = 8;
                 List<string> imagePaths = ImageProcessor.ReadImageDirectory(fullImageDirectory);
                 if (Directory.Exists(fullResultPath))
                 {
@@ -22,21 +22,24 @@
                 var dir = Directory.CreateDirectory(fullResultPath);
                 string resultPath = dir.FullName;
                 var start = DateTime.Now;
-                imagePaths.AsParallel().WithDegreeOfParallelism(threadCount).ForAll(imagePath =>
-                {
-                    ImageProcessor.GrayscaleImage(imagePath, fullResultPath);
-                });
-                
-                //foreach (var imagePath in imagePaths)
+                //imagePaths.AsParallel().WithDegreeOfParallelism(threadCount).ForAll(imagePath =>
                 //{
                 //    ImageProcessor.GrayscaleImage(imagePath, fullResultPath);
-                //}
-                var end = DateTime.Now;
-                Console.WriteLine((end - start).TotalSeconds);
-                timings.Add(end - start);
-            }
+                //});
 
+                foreach (var imagePath in imagePaths)
+                {
+                    ImageProcessor.GrayscaleImage(imagePath, fullResultPath);
+                }
+                var end = DateTime.Now;
+                timings.Add(end - start);
+                Console.WriteLine(end - start);
+            }
+            Console.WriteLine("average:");
+            if (timings.Count > 1) timings = timings.Skip(1).ToList();
             Console.WriteLine(timings.Select(t => t.TotalMilliseconds).Average());
+            Console.WriteLine(timings.Select(t => t.TotalSeconds).Average());
+
         }
     }
 }
