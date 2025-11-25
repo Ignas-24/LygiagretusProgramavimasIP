16438d3d0c9ada72c660769647ee89c58fed5211
ignas24 <ignaspyrantas@gmail.com>
2025-11-25 04:26:07 +0200

    main changes

--- LygiagretusProgramavimasIP/Program.cs
+++ LygiagretusProgramavimasIP/Program.cs
@@ -1,45 +1,47 @@
-﻿namespace LygiagretusProgramavimasIP
+﻿using System.Diagnostics;
+
+namespace LygiagretusProgramavimasIP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            List<TimeSpan> timings = new List<TimeSpan>();
-            int runCount = 2;
-            for (int i = 0; i < runCount; i++)
+            for(int threadCount = 1; threadCount <= 16; threadCount++)
             {
-                string projectDir = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(AppContext.BaseDirectory).FullName).FullName).FullName).FullName;
-                string imageDirectory = "TestCases\\Default";
-                string resultDirectory = "Results";
-                var fullResultPath = Path.Combine(projectDir, resultDirectory);
-                var fullImageDirectory = Path.Combine(projectDir, imageDirectory);
-                int threadCount = 8;
-                List<string> imagePaths = ImageProcessor.ReadImageDirectory(fullImageDirectory);
-                if (Directory.Exists(fullResultPath))
+                List<TimeSpan> timings = new List<TimeSpan>();
+                int runCount = 5;
+               
+                for (int i = 0; i < runCount; i++)
                 {
-                    Directory.Delete(fullResultPath, true);
-                }
-                var dir = Directory.CreateDirectory(fullResultPath);
-                string resultPath = dir.FullName;
-                var start = DateTime.Now;
-                //imagePaths.AsParallel().WithDegreeOfParallelism(threadCount).ForAll(imagePath =>
-                //{
-                //    ImageProcessor.GrayscaleImage(imagePath, fullResultPath);
-                //});
+                    var start = DateTime.Now;
+                    string projectDir = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(AppContext.BaseDirectory).FullName).FullName).FullName).FullName;
+                    string imageDirectory = "TestCases\\5";
+                    string resultDirectory = "Results";
+                    var fullResultPath = Path.Combine(projectDir, resultDirectory);
+                    var fullImageDirectory = Path.Combine(projectDir, imageDirectory);
+                    List<string> imagePaths = ImageProcessor.ReadImageDirectory(fullImageDirectory);
+                    if (Directory.Exists(fullResultPath))
+                    {
+                        Directory.Delete(fullResultPath, true);
+                    }
+                    var dir = Directory.CreateDirectory(fullResultPath);
+                    string resultPath = dir.FullName;
 
-                foreach (var imagePath in imagePaths)
-                {
-                    ImageProcessor.GrayscaleImage(imagePath, fullResultPath);
+                    imagePaths.AsParallel().WithDegreeOfParallelism(threadCount).ForAll(imagePath =>
+                    {
+                        ImageProcessor.GrayscaleImage(imagePath, fullResultPath);
+                    });
+
+                    //foreach (var imagePath in imagePaths)
+                    //{
+                    //    ImageProcessor.GrayscaleImage(imagePath, fullResultPath);
+                    //}
+                    var end = DateTime.Now;
+                    timings.Add(end - start);
                 }
-                var end = DateTime.Now;
-                timings.Add(end - start);
-                Console.WriteLine(end - start);
+                if (timings.Count > 1) timings = timings.Skip(1).ToList();
+                Console.WriteLine($"{threadCount}   {timings.Select(t => t.TotalMilliseconds).Average()}");
             }
-            Console.WriteLine("average:");
-            if (timings.Count > 1) timings = timings.Skip(1).ToList();
-            Console.WriteLine(timings.Select(t => t.TotalMilliseconds).Average());
-            Console.WriteLine(timings.Select(t => t.TotalSeconds).Average());
-
         }
     }
 }
